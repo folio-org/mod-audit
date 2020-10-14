107e6148567335ff4f3bb410f3d2114ad7416b07
khandramai <Viachaslau_Khandramai@epam.com>
2020-10-14 12:20:55 +0300

    MODAUD-18

--- mod-audit-server/src/main/java/org/folio/builder/record/ManualBlockRecordBuilder.java
+++ mod-audit-server/src/main/java/org/folio/builder/record/ManualBlockRecordBuilder.java
@@ -1,26 +1,24 @@
 package org.folio.builder.record;
 
-import io.vertx.core.json.JsonObject;
-import org.folio.builder.description.ManualBlockDescriptionBuilder;
-import org.folio.rest.jaxrs.model.LinkToIds;
-import org.folio.rest.jaxrs.model.LogRecord;
-import org.folio.util.JsonPropertyFetcher;
+import static org.folio.builder.LogRecordBuilderResolver.MANUAL_BLOCK_CREATED;
+import static org.folio.builder.LogRecordBuilderResolver.MANUAL_BLOCK_DELETED;
+import static org.folio.builder.LogRecordBuilderResolver.MANUAL_BLOCK_MODIFIED;
+import static org.folio.util.JsonPropertyFetcher.getObjectProperty;
+import static org.folio.util.JsonPropertyFetcher.getProperty;
+import static org.folio.util.LogEventPayloadField.LOG_EVENT_TYPE;
+import static org.folio.util.LogEventPayloadField.PAYLOAD;
+import static org.folio.util.LogEventPayloadField.USER_ID;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.folio.builder.LogRecordBuilderResolver.MANUAL_BLOCK_CREATED;
-import static org.folio.builder.LogRecordBuilderResolver.MANUAL_BLOCK_DELETED;
-import static org.folio.builder.LogRecordBuilderResolver.MANUAL_BLOCK_MODIFIED;
-import static org.folio.util.JsonPropertyFetcher.getNestedStringProperty;
-import static org.folio.util.JsonPropertyFetcher.getObjectProperty;
-import static org.folio.util.JsonPropertyFetcher.getProperty;
-import static org.folio.util.LogEventPayloadField.BARCODE;
-import static org.folio.util.LogEventPayloadField.LOG_EVENT_TYPE;
-import static org.folio.util.LogEventPayloadField.PAYLOAD;
-import static org.folio.util.LogEventPayloadField.REQUESTER;
-import static org.folio.util.LogEventPayloadField.USER_ID;
+import org.folio.builder.description.ManualBlockDescriptionBuilder;
+import org.folio.rest.jaxrs.model.LinkToIds;
+import org.folio.rest.jaxrs.model.LogRecord;
+import org.folio.util.JsonPropertyFetcher;
+
+import io.vertx.core.json.JsonObject;
 
 public class ManualBlockRecordBuilder implements LogRecordBuilder {
 
@@ -33,11 +31,9 @@ public class ManualBlockRecordBuilder implements LogRecordBuilder {
 
     LogRecord manualBlockLogRecord = new LogRecord().withObject(LogRecord.Object.MANUAL_BLOCK)
       .withAction(resolveLogRecordAction(logEventType))
-      .withUserBarcode(getNestedStringProperty(manualBlockJson, REQUESTER, BARCODE))
       .withDate(new Date())
       .withDescription(new ManualBlockDescriptionBuilder().buildDescription(manualBlockJson))
-      .withLinkToIds(new LinkToIds()
-        .withUserId(JsonPropertyFetcher.getProperty(manualBlockJson, USER_ID)));
+      .withLinkToIds(new LinkToIds().withUserId(JsonPropertyFetcher.getProperty(manualBlockJson, USER_ID)));
 
     logRecords.add(manualBlockLogRecord);
 
