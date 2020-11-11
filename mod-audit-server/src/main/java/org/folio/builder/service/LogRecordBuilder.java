20719a07c236fd8a7954372d64d235372cdedb3e
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2020-11-11 12:57:56 +0300

    MODAUD-31 - Requests log actions - improvements (#58)
    * MODAUD-31
    
    * MODAUD-31
    
    * MODAUD-31
    
    * Update mod-audit-server/src/main/java/org/folio/builder/service/LogRecordBuilder.java
    
    Co-authored-by: siarhei-charniak <siarhei_charniak@epam.com>
    
    Co-authored-by: siarhei-charniak <siarhei_charniak@epam.com>
    (cherry picked from commit df04c83dc177c3e616bc2d51cbc28a05454276da)

--- mod-audit-server/src/main/java/org/folio/builder/service/LogRecordBuilder.java
+++ mod-audit-server/src/main/java/org/folio/builder/service/LogRecordBuilder.java
@@ -6,6 +6,7 @@ import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang.StringUtils.EMPTY;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import static org.folio.rest.RestVerticle.OKAPI_HEADER_TENANT;
 import static org.folio.util.Constants.HOLDINGS_URL;
 import static org.folio.util.Constants.ITEMS_URL;
@@ -34,7 +35,6 @@ import static org.folio.util.LogEventPayloadField.USER_ID;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -66,9 +66,12 @@ public abstract class LogRecordBuilder {
   public static final String SEARCH_PARAMS = "?limit=%s&offset=%s%s";
   public static final String ID = "id";
   public static final String USERS = "users";
+  public static final String CANCELLATION_REASONS = "cancellationReasons";
 
   protected final Map<String, String> okapiHeaders;
   protected final Context vertxContext;
+
+  public abstract CompletableFuture<List<LogRecord>> buildLogRecord(JsonObject payload);
 
   public LogRecordBuilder(Map<String, String> okapiHeaders, Context vertxContext) {
     this.okapiHeaders = okapiHeaders;
@@ -115,9 +118,9 @@ public abstract class LogRecordBuilder {
    * @param ids List of item id's
    * @return future with list of item records
    */
-  public CompletableFuture<List<JsonObject>> getEntitiesByIds(List<String> ids, String key) {
+  public CompletableFuture<List<JsonObject>> getEntitiesByIds(String url, String key, int limit, int offset, String... ids) {
     String query = convertIdsToCqlQuery(ids);
-    String endpoint = String.format(USERS_URL + SEARCH_PARAMS, 2, 0, buildQuery(query));
+    String endpoint = String.format(url + SEARCH_PARAMS, limit, offset, buildQuery(query));
     return handleGetRequest(endpoint).thenApply(response -> extractEntities(response, key));
   }
 
@@ -140,9 +143,9 @@ public abstract class LogRecordBuilder {
             ofNullable(getProperty(userJson, BARCODE)).ifPresent(barcode -> payload.put(USER_BARCODE.value(), barcode));
           }
           JsonObject personal = getObjectProperty(userJson, PERSONAL);
-          if (nonNull(personal)) {
+          if (nonNull(personal) && nonNull(buildPersonalName(getProperty(personal, FIRST_NAME), getProperty(personal, LAST_NAME)))) {
             payload.put(PERSONAL_NAME.value(),
-              String.format(PERSONAL_NAME_PATTERN, getProperty(personal, LAST_NAME), getProperty(personal, FIRST_NAME)));
+              buildPersonalName(getProperty(personal, FIRST_NAME), getProperty(personal, LAST_NAME)));
           }
         }
         return CompletableFuture.completedFuture(payload);
@@ -194,8 +197,8 @@ public abstract class LogRecordBuilder {
    * @param ids list of id's
    * @return String representing CQL query to get records by id's
    */
-  private String convertIdsToCqlQuery(Collection<String> ids) {
-    return convertIdsToCqlQuery(ids, ID, true);
+  private String convertIdsToCqlQuery(String... ids) {
+    return convertIdsToCqlQuery(ID, true, ids);
   }
 
   /**
@@ -206,7 +209,7 @@ public abstract class LogRecordBuilder {
    * @param strictMatch indicates whether strict match mode (i.e. ==) should be used or not (i.e. =)
    * @return String representing CQL query to get records by some property values
    */
-  private String convertIdsToCqlQuery(Collection<String> values, String fieldName, boolean strictMatch) {
+  private String convertIdsToCqlQuery(String fieldName, boolean strictMatch, String... values) {
     String prefix = fieldName + (strictMatch ? "==(" : "=(");
     return StreamEx.of(values)
       .joining(" or ", prefix, ")");
@@ -244,13 +247,23 @@ public abstract class LogRecordBuilder {
     return response.getBody();
   }
 
-  public abstract CompletableFuture<List<LogRecord>> buildLogRecord(JsonObject payload);
-
   protected LogRecord.Action resolveAction(String actionString) {
     try {
       return LogRecord.Action.fromValue(actionString);
     } catch (IllegalArgumentException e) {
       throw new IllegalArgumentException("Builder isn't implemented yet for: " + actionString);
+    }
+  }
+
+  String buildPersonalName(String firstName, String lastName) {
+    if (isNotEmpty(firstName) && isNotEmpty(lastName)) {
+      return lastName + ", " + firstName;
+    } else if (isEmpty(firstName) && isNotEmpty(lastName)) {
+      return lastName;
+    } else if (isNotEmpty(firstName) && isEmpty(lastName)) {
+      return firstName;
+    } else {
+      return null;
     }
   }
 }
--- mod-audit-server/src/main/java/org/folio/util/Constants.java
+++ mod-audit-server/src/main/java/org/folio/util/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
   public static final String HOLDINGS_URL = "/holdings-storage/holdings";
   public static final String TEMPLATES_URL = "/templates";
   public static final String USERS_URL = "/users";
+  public static final String CANCELLATION_REASONS_URL = "/cancellation-reason-storage/cancellation-reasons";
   public static final String URL_WITH_ID_PATTERN = "%s/%s";
   public static final String SYSTEM = "System";
   public static final String UUID_PATTERN = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$";
