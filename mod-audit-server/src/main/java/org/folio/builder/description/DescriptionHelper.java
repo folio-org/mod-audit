202f3b50b7caa723f9845296f4ac5cfebeb03ad4
khandramai <Viachaslau_Khandramai@epam.com>
2021-04-05 14:58:15 +0300

    MODAUD-64 # Logic improvements

--- mod-audit-server/src/main/java/org/folio/builder/description/DescriptionHelper.java
+++ mod-audit-server/src/main/java/org/folio/builder/description/DescriptionHelper.java
@@ -5,7 +5,7 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.Objects;
 
@@ -14,16 +14,19 @@ public class DescriptionHelper {
   private DescriptionHelper() {
   }
 
-  private static final DateTimeFormatter FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
-  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+  public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+  private static final DateTimeFormatter JODA_TIME_FORMATTER = DateTimeFormat.forPattern(DATE_PATTERN);
+  private static final java.time.format.DateTimeFormatter LOCAL_DATE_TIME_FORMATTER = java.time.format.DateTimeFormatter.ofPattern(DATE_PATTERN);
 
   public static String getFormattedDateTime(DateTime dateTime) {
-    return dateTime.toString(FORMAT);
+    return dateTime.toString(JODA_TIME_FORMATTER);
   }
 
   public static String getFormattedDateTime(Date date) {
     if (Objects.nonNull(date)) {
-      return DATE_FORMAT.format(date);
+      return LOCAL_DATE_TIME_FORMATTER.format(date.toInstant()
+        .atZone(ZoneId.systemDefault())
+        .toLocalDateTime());
     }
     return StringUtils.EMPTY;
   }
