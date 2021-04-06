030f5b3a4e89f077d58ff912ccd5d28b1ba9b330
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2021-04-06 10:48:04 +0300

    MODAUD-64 - Filter circulation log on description is not working (#78)
    * MODAUD-66
    # Loss userId fixing
    
    * MODAUD-64
    # Logic improvements
    
    * MODAUD-64
    # Logic improvements
    
    * MODAUD-64
    # Logic improvements
    
    * MODAUD-64
    # Logic improvements
    
    * MODAUD-64
    # Logic improvements
    
    * MODAUD-64
    # Logic improvements

--- mod-audit-server/src/main/java/org/folio/builder/description/DescriptionHelper.java
+++ mod-audit-server/src/main/java/org/folio/builder/description/DescriptionHelper.java
@@ -15,9 +15,10 @@ public class DescriptionHelper {
   private DescriptionHelper() {
   }
 
-  public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
-  private static final DateTimeFormatter JODA_TIME_FORMATTER = DateTimeFormat.forPattern(DATE_PATTERN);
-  private static final java.time.format.DateTimeFormatter DATE_TIME_FORMATTER = java.time.format.DateTimeFormatter.ofPattern(DATE_PATTERN);
+  public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+  public static final String DATE_PATTERN = "yyyy-MM-dd";
+  private static final DateTimeFormatter JODA_TIME_FORMATTER = DateTimeFormat.forPattern(DATE_TIME_PATTERN);
+  private static final java.time.format.DateTimeFormatter DATE_FORMATTER = java.time.format.DateTimeFormatter.ofPattern(DATE_PATTERN);
 
   public static String getFormattedDateTime(DateTime dateTime) {
     return dateTime.toString(JODA_TIME_FORMATTER);
@@ -26,7 +27,7 @@ public class DescriptionHelper {
   public static String getFormattedDateTime(Date date) {
     if (Objects.nonNull(date)) {
       return LocalDateTime.ofInstant(date.toInstant(), ZoneId.of("GMT"))
-        .atZone(ZoneId.systemDefault()).format(DATE_TIME_FORMATTER);
+        .atZone(ZoneId.systemDefault()).format(DATE_FORMATTER);
     }
     return StringUtils.EMPTY;
   }
