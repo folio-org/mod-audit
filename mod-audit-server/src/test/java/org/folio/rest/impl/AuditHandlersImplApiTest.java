9e3e884a31504c4beeeba6eb8ec1bc364ebc9a6b
viachaslau_khandramai <Viachaslau_Khandramai@epam.com>
2022-12-05 13:46:17 +0100

    MODAUD-148 - Edited request record is not correctly stored in the Circulation log (update formatter)

--- mod-audit-server/src/test/java/org/folio/rest/impl/AuditHandlersImplApiTest.java
+++ mod-audit-server/src/test/java/org/folio/rest/impl/AuditHandlersImplApiTest.java
@@ -11,6 +11,7 @@ import static org.folio.utils.TenantApiTestUtil.NOTICE_ERROR_FULL_PAYLOAD_JSON;
 import static org.folio.utils.TenantApiTestUtil.REQUEST_CREATED_PAYLOAD_JSON;
 import static org.folio.utils.TenantApiTestUtil.REQUEST_CREATED_THROUGH_OVERRIDE_PAYLOAD_JSON;
 import static org.folio.utils.TenantApiTestUtil.REQUEST_EDITED_PAYLOAD_JSON;
+import static org.folio.utils.TenantApiTestUtil.REQUEST_EDITED_PAYLOAD_WITH_NON_EMPTY_DATE_JSON;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.folio.utils.TenantApiTestUtil.CHECK_IN_PAYLOAD_JSON;
 import static org.folio.utils.TenantApiTestUtil.getFile;
@@ -68,7 +69,7 @@ public class AuditHandlersImplApiTest extends ApiTestBase {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {REQUEST_CREATED_THROUGH_OVERRIDE_PAYLOAD_JSON, REQUEST_EDITED_PAYLOAD_JSON})
+  @ValueSource(strings = {REQUEST_CREATED_THROUGH_OVERRIDE_PAYLOAD_JSON, REQUEST_EDITED_PAYLOAD_JSON, REQUEST_EDITED_PAYLOAD_WITH_NON_EMPTY_DATE_JSON})
   void postLogRecordEventForRequestOverride(String sample) {
     logger.info("post valid log event for request creation through override: success");
 
