07a37e2e4d101c77784e3ef71cbb8fcd1304de08
Serhii Nosko <Serhii_Nosko@epam.com>
2022-12-19 09:48:04 +0200

    MODAUD-144. Closing promise in close of failures

--- mod-audit-server/src/main/java/org/folio/verticle/acquisition/consumers/OrderLineEventsHandler.java
+++ mod-audit-server/src/main/java/org/folio/verticle/acquisition/consumers/OrderLineEventsHandler.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.kafka.AsyncRecordHandler;
 import org.folio.kafka.KafkaHeaderUtils;
+import org.folio.kafka.exception.DuplicateEventException;
 import org.folio.rest.jaxrs.model.OrderLineAuditEvent;
 import org.folio.rest.util.OkapiConnectionParams;
 import org.folio.services.acquisition.OrderLineAuditEventsService;
@@ -38,11 +39,27 @@ public class OrderLineEventsHandler implements AsyncRecordHandler<String, String
     Promise<String> result = Promise.promise();
     List<KafkaHeader> kafkaHeaders = record.headers();
     OkapiConnectionParams okapiConnectionParams = new OkapiConnectionParams(KafkaHeaderUtils.kafkaHeadersToMap(kafkaHeaders), vertx);
-    OrderLineAuditEvent orderLineAuditEvent = new JsonObject(record.value()).mapTo(OrderLineAuditEvent.class);
-    LOGGER.debug("Event was received with event type: {}", orderLineAuditEvent.getAction());
+    OrderLineAuditEvent event = new JsonObject(record.value()).mapTo(OrderLineAuditEvent.class);
+    LOGGER.info("Starting processing of Order Line audit event with id: {} for order id: {} and order line id: {}",
+      event.getId(), event.getOrderId(), event.getOrderLineId());
 
-    orderLineAuditEventsService.saveOrderLineAuditEvent(orderLineAuditEvent, okapiConnectionParams.getTenantId())
-      .onSuccess(ar-> result.complete());
+    orderLineAuditEventsService.saveOrderLineAuditEvent(event, okapiConnectionParams.getTenantId())
+      .onSuccess(ar -> {
+        LOGGER.info("Order Line audit event with id: {} has been processed for order id: {} and order line id: {}",
+          event.getId(), event.getOrderId(), event.getOrderLineId());
+        result.complete(event.getId());
+      })
+      .onFailure(e -> {
+        if (e instanceof DuplicateEventException) {
+          LOGGER.info("Duplicate Order Line audit event with id: {} for order id: {} and order line id: {} received, skipped processing",
+            event.getId(), event.getOrderId(), event.getOrderLineId());
+          result.complete(event.getId());
+        } else {
+          LOGGER.error("Processing of Order Line audit event with id: {} for order id: {} and order line id: {} has been failed",
+            event.getId(), event.getOrderId(), event.getOrderLineId(), e);
+          result.fail(e);
+        }
+      });
 
     return result.future();
   }
