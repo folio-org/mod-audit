af2786338330e8a2d456ef28efce7b92e25ad1ca
azizbekxm <azizbek_khushvakov@epam.com>
2023-11-15 17:29:25 +0500

    [MODAUD-174] - Minor improvement

--- mod-audit-server/src/main/java/org/folio/util/DbUtils.java
+++ mod-audit-server/src/main/java/org/folio/util/DbUtils.java
@@ -4,6 +4,8 @@ import static java.lang.String.format;
 import static org.folio.rest.persist.PostgresClient.convertToPsqlStandard;
 
 public class DbUtils {
+  private DbUtils() {
+  }
 
   public static String formatDBTableName(String tenantId, String table) {
     return format("%s.%s", convertToPsqlStandard(tenantId), table);
