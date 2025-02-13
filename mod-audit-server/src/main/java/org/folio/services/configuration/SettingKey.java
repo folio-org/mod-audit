2c579dbf47c343d9f2437d2953cbfaaaa9df6cc5
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2025-02-13 16:26:21 +0200

    feat[setting] retention Period Configuration (#196)

---
+++ mod-audit-server/src/main/java/org/folio/services/configuration/SettingKey.java
@@ -0,0 +1,16 @@
+package org.folio.services.configuration;
+
+import lombok.Getter;
+
+
+@Getter
+public enum SettingKey {
+  RECORDS_PAGE_SIZE("records.page.size"),
+  RETENTION_PERIOD("records.retention.period");
+
+  private final String value;
+
+  SettingKey(String value) {
+    this.value = value;
+  }
+}
